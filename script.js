b368ee6933a57ff6134298316cf449e9dc8cce5a
Thaís Cristine Coelho <thaisholz@hotmail.com>
2025-02-11 14:49:22 +0000

    Criando elementos da lista e conectando eles entre si

---
+++ script.js
@@ -0,0 +1,31 @@
+const inputItem = document.getElementById('input-item');
+const listaDeCompras = document.getElementById('lista-de-compras');
+const botaoAdicionar = document.getElementById('adicionar-item');
+let contador = 0;
+
+botaoAdicionar.addEventListener('click', function(evento) {
+    evento.preventDefault();
+    if (inputItem.value === ""){
+        alert('Por favor, insira um item!');
+        return;
+    }
+
+    const itemDaLista = document.createElement('li');
+    const containerItemDaLista = document.createElement('div');
+    containerItemDaLista.classList.add('lista-item-container');
+
+    const inputCheckbox = document.createElement('input');
+    inputCheckbox.type = 'checkbox';
+    inputCheckbox.id = 'checkbox-' + contador++;
+
+    const nomeItem = document.createElement('p');
+    nomeItem.innerHTML = inputItem.value;
+
+    //Conectando os itens de 'filho para pai'
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
+
+    itemDaLista.appendChild(containerItemDaLista);
+    listaDeCompras.appendChild(itemDaLista);
+
+})
