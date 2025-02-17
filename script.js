561a6908f8cbe4dcafa18559f306c84d69e39c56
Thaís Cristine Coelho <thaisholz@hotmail.com>
2025-02-17 13:44:44 +0000

    Criando data e hora nos itens e add interação de estilo

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ botaoAdicionar.addEventListener('click', function(evento) {
         return;
     }
 
+    //Criando item na lista
     const itemDaLista = document.createElement('li');
     const containerItemDaLista = document.createElement('div');
     containerItemDaLista.classList.add('lista-item-container');
@@ -21,11 +22,31 @@ botaoAdicionar.addEventListener('click', function(evento) {
     const nomeItem = document.createElement('p');
     nomeItem.innerHTML = inputItem.value;
 
+    //Add interação de riscar o nome da lista
+    inputCheckbox.addEventListener('click', function(){
+        if (inputCheckbox.checked){
+            nomeItem.style.textDecoration = 'line-through';
+        }else {
+            nomeItem.style.textDecoration = 'none';
+        }
+    });
+
     //Conectando os itens de 'filho para pai'
     containerItemDaLista.appendChild(inputCheckbox);
     containerItemDaLista.appendChild(nomeItem);
 
     itemDaLista.appendChild(containerItemDaLista);
+
+    //Para colocar data e hora
+    const diaDaSemana = new Date().toLocaleDateString('pt-BR', {weekday: 'long'});
+    const data = new Date().toLocaleDateString('pt-BR');
+    const hora = new Date().toLocaleTimeString('pt-BR', {hour: 'numeric', minute: 'numeric'});
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+    const itemData = document.createElement('p');
+
+    itemData.innerText = dataCompleta;
+    itemData.classList.add('texto-data'); //Add o estilo das letras
+    itemDaLista.appendChild(itemData);
     listaDeCompras.appendChild(itemDaLista);
 
 })
