0be21263c3fe91188a88ef4438e40c24617d5ecc
Thaís Cristine Coelho <thaisholz@hotmail.com>
2025-02-17 14:49:59 +0000

    Modularizando o código JS

--- script.js
+++ script.js
@@ -1,52 +1,25 @@
-const inputItem = document.getElementById('input-item');
+import { criarItemDaLista } from './scripts/criarItemDaLista.js';
+
 const listaDeCompras = document.getElementById('lista-de-compras');
 const botaoAdicionar = document.getElementById('adicionar-item');
-let contador = 0;
 
 botaoAdicionar.addEventListener('click', function(evento) {
     evento.preventDefault();
-    if (inputItem.value === ""){
-        alert('Por favor, insira um item!');
-        return;
-    }
-
-    //Criando item na lista
-    const itemDaLista = document.createElement('li');
-    const containerItemDaLista = document.createElement('div');
-    containerItemDaLista.classList.add('lista-item-container');
-
-    const inputCheckbox = document.createElement('input');
-    inputCheckbox.type = 'checkbox';
-    inputCheckbox.id = 'checkbox-' + contador++;
-
-    const nomeItem = document.createElement('p');
-    nomeItem.innerHTML = inputItem.value;
-
-    //Add interação de riscar o nome da lista
-    inputCheckbox.addEventListener('click', function(){
-        if (inputCheckbox.checked){
-            nomeItem.style.textDecoration = 'line-through';
-        }else {
-            nomeItem.style.textDecoration = 'none';
-        }
-    });
-
-    //Conectando os itens de 'filho para pai'
-    containerItemDaLista.appendChild(inputCheckbox);
-    containerItemDaLista.appendChild(nomeItem);
-
-    itemDaLista.appendChild(containerItemDaLista);
-
-    //Para colocar data e hora
-    const diaDaSemana = new Date().toLocaleDateString('pt-BR', {weekday: 'long'});
-    const data = new Date().toLocaleDateString('pt-BR');
-    const hora = new Date().toLocaleTimeString('pt-BR', {hour: 'numeric', minute: 'numeric'});
-    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
-    const itemData = document.createElement('p');
-
-    itemData.innerText = dataCompleta;
-    itemData.classList.add('texto-data'); //Add o estilo das letras
-    itemDaLista.appendChild(itemData);
+    const itemDaLista = criarItemDaLista();
     listaDeCompras.appendChild(itemDaLista);
-
+    verificarListaVazia();
 })
+
+//Mensagem de lista vazia
+const msgListaVazia = document.querySelector('.msg-lista-vazia');
+
+function verificarListaVazia(){
+    const elementosDaLista = listaDeCompras.querySelectorAll('li');
+    if (elementosDaLista.length === 0){
+        msgListaVazia.style.display = 'block';
+    }else {
+        msgListaVazia.style.display = 'none';
+    }
+}
+
+verificarListaVazia();
---
+++ scripts/criarItemDaLista.js
@@ -0,0 +1,46 @@
+import gerarDiaDaSemana from './gerarDiaDaSemana.js';
+const inputItem = document.getElementById('input-item');
+let contador = 0;
+
+export function criarItemDaLista(){
+    if (inputItem.value === ""){
+        alert('Por favor, insira um item!');
+        return;
+    }
+
+    //Criando item na lista
+    const itemDaLista = document.createElement('li');
+    const containerItemDaLista = document.createElement('div');
+    containerItemDaLista.classList.add('lista-item-container');
+
+    const inputCheckbox = document.createElement('input');
+    inputCheckbox.type = 'checkbox';
+    inputCheckbox.id = 'checkbox-' + contador++;
+
+    const nomeItem = document.createElement('p');
+    nomeItem.innerHTML = inputItem.value;
+
+    //Add interação de riscar o nome da lista
+    inputCheckbox.addEventListener('click', function(){
+        if (inputCheckbox.checked){
+            nomeItem.style.textDecoration = 'line-through';
+        }else {
+            nomeItem.style.textDecoration = 'none';
+        }
+    });
+
+    //Conectando os itens de 'filho para pai'
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
+
+    itemDaLista.appendChild(containerItemDaLista);
+    const dataCompleta = gerarDiaDaSemana();
+    
+    const itemData = document.createElement('p');
+
+    itemData.innerText = dataCompleta;
+    itemData.classList.add('texto-data'); //Add o estilo das letras
+    itemDaLista.appendChild(itemData);
+
+    return itemDaLista;
+}
